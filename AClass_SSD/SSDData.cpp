5eab3826406922d55752f41254a8fc98e85ffe39
heejeong <irene725k@gmail.com>
2025-04-16 20:49:34 +0900

    [refactor]  readLBA and initSSDDataAndReadLBA test

--- AClass_SSD/SSDData.cpp
+++ AClass_SSD/SSDData.cpp
@@ -1,4 +1,5 @@
 #include "SSDData.h"
+
 #include <sstream>
 
 SSDData::SSDData(DataInterface* data) : data(data) {}
@@ -6,16 +7,19 @@ SSDData::SSDData(DataInterface* data) : data(data) {}
 void SSDData::writeLBA(int lba, uint32_t value) { return; }
 
 unsigned int SSDData::readLBA(int lba) {
-  if (ssdData.empty()) {
-    std::string rawData = data->loadFromFile();
-    std::istringstream iss(rawData);
-    std::string line;
-    unsigned int value;
+	if (ssdData.empty()) getSsdData();
 
-    while (std::getline(iss, line)) {
-      value = std::stoul(line, nullptr, 16);
-      ssdData.push_back(value);
-    }
-  }
-  return ssdData[lba];
+	return ssdData[lba];
+}
+
+void SSDData::getSsdData() {
+	std::string rawData = data->loadFromFile();
+	std::istringstream iss(rawData);
+	std::string line;
+	unsigned int value;
+
+	while (std::getline(iss, line)) {
+		value = std::stoul(line, nullptr, 16);
+		ssdData.push_back(value);
+	}
 }
--- AClass_SSD/SSDDataTest.cpp
+++ AClass_SSD/SSDDataTest.cpp
@@ -1,23 +1,32 @@
 #include <gmock/gmock.h>
+
 #include <string>
 
-#include "SSDData.h"
 #include "DataInterface.h"
+#include "SSDData.h"
+
+using namespace ::testing;
 
 class DataMock : public DataInterface {
- public:
-  DataMock(std::string fileName) : DataInterface(fileName) {}
-  MOCK_METHOD(std::string, loadFromFile, (), (override));
-  MOCK_METHOD(void, saveToFile, (std::string), (const, override));
+public:
+	DataMock(std::string fileName) : DataInterface(fileName) {}
+	MOCK_METHOD(std::string, loadFromFile, (), (override));
+	MOCK_METHOD(void, saveToFile, (std::string), (const, override));
+};
+class SSDDataTest : public Test {
+public:
+	std::string data = "0xffffff10\n0x00000020";
+	std::vector<unsigned int> golden = { 0xffffff10 ,0x00000020 };
+	DataMock dataMock;
+	SSDData ssd;
+
+	SSDDataTest() : dataMock("test.txt"), ssd(&dataMock) {}
 };
 
-TEST(SSDDataTest, initSSDDataAndReadLBA) {
-  std::string data = "0xffffff10\n0x00000020";
-  DataMock dataMock("test.txt");
-  SSDData ssd(&dataMock);
+TEST_F(SSDDataTest, initSSDDataAndReadLBA) {
 
-  EXPECT_CALL(dataMock, loadFromFile()).WillRepeatedly(::testing::Return(data));
+	EXPECT_CALL(dataMock, loadFromFile()).WillRepeatedly(Return(data));
 
-  EXPECT_EQ(ssd.readLBA(0), 0xffffff10);
-  EXPECT_EQ(ssd.readLBA(1), 0x00000020);
+	EXPECT_EQ(ssd.readLBA(0), golden[0]);
+	EXPECT_EQ(ssd.readLBA(1), golden[1]);
 }
