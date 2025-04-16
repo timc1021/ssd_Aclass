ce4fa8655378512c85d941521a61d2567380c1dd
KimHeejeong <rgw133@naver.com>
2025-04-16 15:22:29 +0900

    [feature] Create basic structure for Data class
    Signed-off-by: KimHeejeong <rgw133@naver.com>

--- AClass_SSD/Data.h
+++ AClass_SSD/Data.h
@@ -2,6 +2,10 @@
 #include <vector>
 
 class Data {
-	std::vector<unsigned int> data;
+	std::vector<uint32_t> nandData;
 public:
+	static const int LBA_SIZE = 100;
+
+	bool loadFromFile(const std::string& filename);
+	bool saveToFile(const std::string& filename) const;
 };
