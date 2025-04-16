59e03c556161ffba62fb9ed759d844a3ffa38f50
Jaemin Choi <jaeminy.choi@samsung.com>
2025-04-16 16:19:24 +0900

    [refac] refactoring parsing command
    - Change function name to be more readable
    - Change TDD code to call real write() and read() function.
    
    Signed-off-by: Jaemin Choi <jaeminy.choi@samsung.com>

--- Test_Shell/TestShell.h
+++ Test_Shell/TestShell.h
@@ -1,12 +1,12 @@
 #pragma once
 #include <vector>
-
+#include <string>
 using std::string;
 using std::vector;
 
 class TestShell {
 public:
-	unsigned int init(string input);
+	int handleCommand(string input);
 	vector<string> splitBySpace(const string& input);
 	int write(int lba, uint32_t data);
 	int read(int lba);
