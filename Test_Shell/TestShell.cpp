96cd263091059f44b733b7bf7f6324bf299e8391
Jaemin Choi <jaeminy.choi@samsung.com>
2025-04-16 15:20:44 +0900

    [test] add TC for parsing command
    Add init function to handle command from user.
    Init function seperates words by space
    and branches to proper command(write/read).
    
    Signed-off-by: Jaemin Choi <jaeminy.choi@samsung.com>

--- Test_Shell/TestShell.cpp
+++ Test_Shell/TestShell.cpp
@@ -1,3 +1,33 @@
+#include <string>
+#include <vector>
 class TestShell {
 public:
+    std::vector<std::string> splitBySpace(const std::string& input) {
+        std::vector<std::string> tokens;
+        size_t start = 0, end;
+
+        while ((end = input.find(' ', start)) != std::string::npos) {
+            if (end != start)
+                tokens.push_back(input.substr(start, end - start));
+            start = end + 1;
+        }
+
+        if (start < input.size())
+            tokens.push_back(input.substr(start));
+
+        return tokens;
+    }
+
+    unsigned int init(std::string input) {
+        std::vector<std::string> result = splitBySpace(input);
+
+        if (result[0] == "read") {
+            // call read()
+            return true;
+        }
+        else if (result[0] == "write") {
+            // call write()
+            return true;
+        }
+    }
 };
