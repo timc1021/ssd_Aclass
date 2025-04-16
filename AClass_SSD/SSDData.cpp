1944bed33661e54a0023648778c31c24031a4f0a
heejeong <irene725k@gmail.com>
2025-04-16 21:20:12 +0900

    [feature] Throw Exception When ReadLBA With Invalid LBA Value

--- AClass_SSD/SSDData.cpp
+++ AClass_SSD/SSDData.cpp
@@ -1,14 +1,15 @@
 #include "SSDData.h"
 
 #include <sstream>
+#include <exception>
 
 SSDData::SSDData(DataInterface* data) : data(data) {}
 
 void SSDData::writeLBA(int lba, uint32_t value) { return; }
 
 unsigned int SSDData::readLBA(int lba) {
+	if (lba <0 || lba >LBA_SIZE) throw std::invalid_argument("LBA must be greater than or equal to 0 and less than 100");
 	if (ssdData.empty()) getSsdData();
-
 	return ssdData[lba];
 }
 
@@ -18,7 +19,7 @@ void SSDData::getSsdData() {
 	std::string line;
 	unsigned int value;
 
-	if (rawData == "") ssdData.resize(100, 0x00000000);
+	if (rawData == "") ssdData.resize(LBA_SIZE, 0x00000000);
 
 	while (std::getline(iss, line))
 		ssdData.push_back(std::stoul(line, nullptr, 16));
