dad04ac945bad7305e987970c91aa0d9e4911bc9
yujung.choi <unichoi92@hanmail.net>
2025-04-17 15:55:58 +0900

    [refac] parsing 함수 리팽토링 등 - token 인덱스 enum화 - TestShellScript.cpp 파일 미사용으로 삭제 - help 메시지 수정

--- Inc/ITestShell.h
+++ Inc/ITestShell.h
@@ -15,26 +15,42 @@ class ITestShell {
 public:
 	COMMAND_RESULT handleCommand(const string& commandLine);
 	vector<string> splitBySpace(const string& input);
-	void fullWrite(uint32_t data);
+	void fullWrite(const uint32_t data);
 	uint32_t fullRead();
 	void help();
 	COMMAND_RESULT exit();
 
-	bool readCompare(int lba, uint32_t expected);
+	bool readCompare(const int lba, const uint32_t expected);
 	bool fullWriteAndReadCompare();
 	bool partialLBAWrite();
 	bool writeReadAging();
 
-	virtual void write(int lba, uint32_t data) = 0;
-	virtual uint32_t read(int lba) = 0;
+	virtual void write(const int lba, const uint32_t data) = 0;
+	virtual uint32_t read(const int lba) = 0;
 
 	const int MAX_LBA_SIZE = 100;
+	const int START_LBA = 0;
 
 private:
-	bool isCapitalLetter(const char c);
 	bool isWriteDataValid(const string& commandLine);
-	bool isWriteCommandValid(const string& commandLine);
-	bool isReadCommandValid(const string& commandLine);
-	bool isValidLBA(const vector<string>& commandToken);
-	bool isCommandValid(const string& commandLine);
+	bool isWriteCommandValid(const vector<string> commandToken);
+	bool isReadCommandValid(const vector<string> commandToken);
+	bool isFullwriteCommandValid(const vector<string> commandToken);
+	bool isValidLBA(const string& lba);
+	bool isCommandValid(const vector<string> commandToken);
+
+	const vector<string> commandList = {
+	"read",
+	"write",
+	"fullread",
+	"fullwrite",
+	"help",
+	"exit",
+	"1_FullWriteAndReadCompare",
+	"1_",
+	"2_PartialLBAWrite",
+	"2_",
+	"3_WriteReadAging",
+	"3_",
+	};
 };
