713c22b5750650f47fcba6d5b05ca312e55ca81b
chunghee <boodooboo100@naver.com>
2025-04-17 15:04:24 +0900

    [feature] modify main to make ssd executable

--- AClass_SSD/main.cpp
+++ AClass_SSD/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
-#include "gmock/gmock.h"
+#include <string>
+#include "Command.h"
+#include "SSDController.h"
+#include "FileTextIO.h"
 
-int main() {
-	::testing::InitGoogleMock();
-	return RUN_ALL_TESTS();
+int main(int argc, char* argv[]) {
+
+    std::string cmdType = argv[1];
+    int lba = std::stoi(argv[2]);
+    std::string value = (cmdType == "W" && argc == 4) ? argv[3] : "";
+
+    FileTextIO nandFile("ssd_nand.txt");
+    FileTextIO outputFile("ssd_output.txt");
+    SSDController ssd(&nandFile);
+    Command command(ssd, outputFile);
+    try {
+        command.execute(cmdType, lba, value);
+    }
+    catch (const std::exception& e) {
+        outputFile.saveToFile("ERROR");
+    }
+    return 0;
 }
-
