77606ad0a88a02e18385aeff1b0f47e30497b2e6
yujung.choi <unichoi92@hanmail.net>
2025-04-17 13:13:15 +0900

    [bugfix] shell에서 enter만 입력시 프로그램 죽는 에러 수정

--- Test_Shell/TestShellApp.cpp
+++ Test_Shell/TestShellApp.cpp
@@ -5,9 +5,11 @@ void TestShellApp::run(std::istream& in, std::ostream& out)
 {
     while (true) {
         std::string command;
+
         out << "shell> ";
-        if (!std::getline(in, command))
-            break;
+
+        if (!std::getline(in, command)) break;
+        if (command.empty()) continue;
 
         COMMAND_RESULT result = testShell->handleCommand(command);
         if (result == COMMAND_INVALID_PARAM)
