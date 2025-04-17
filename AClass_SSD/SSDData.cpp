0953506055ba2de6e30a61ae47dbbde9fd28aee6
KimHeejeong <rgw133@naver.com>
2025-04-17 10:11:49 +0900

    [refactor] Improve basic write function

--- AClass_SSD/SSDData.cpp
+++ AClass_SSD/SSDData.cpp
@@ -6,33 +6,26 @@
 
 SSDData::SSDData(DataInterface* data) : data(data) {}
 
-// TODO: implement write logic
 void SSDData::writeLBA(int lba, uint32_t value) {
 	if (lba < 0 || lba >= LBA_SIZE) throw std::invalid_argument("LBA must be greater than or equal to 0 and less than 100");
-	if (ssdData.empty()) getSsdData();
+	if (ssdData.empty()) getSsdDataFromFile();
 
 	ssdData[lba] = value;
 
-	std::stringstream ss;
-
-	for (auto data : ssdData)
-		ss << "0x" << std::hex << std::setw(8) << std::setfill('0') << data << '\n';
-
-	data->saveToFile(ss.str());
+	saveSsdDataToFile();
 	return;
 }
 
 unsigned int SSDData::readLBA(int lba) {
 	if (lba < 0 || lba >= LBA_SIZE) throw std::invalid_argument("LBA must be greater than or equal to 0 and less than 100");
-	if (ssdData.empty()) getSsdData();
+	if (ssdData.empty()) getSsdDataFromFile();
 	return ssdData[lba];
 }
 
-void SSDData::getSsdData() {
+void SSDData::getSsdDataFromFile() {
 	std::string rawData = data->loadFromFile();
 	std::istringstream iss(rawData);
 	std::string line;
-	unsigned int value;
 
 	if (rawData == "") {
 		ssdData.resize(LBA_SIZE, 0x00000000);
@@ -41,4 +34,13 @@ void SSDData::getSsdData() {
 
 	while (std::getline(iss, line))
 		ssdData.push_back(std::stoul(line, nullptr, 16));
+}
+void SSDData::saveSsdDataToFile()
+{
+	std::stringstream ss;
+
+	for (auto data : ssdData)
+		ss << "0x" << std::hex << std::setw(8) << std::setfill('0') << data << '\n';
+
+	data->saveToFile(ss.str());
 }
