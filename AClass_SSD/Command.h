98f580e4176ae5d20a15ade9140f9268f142c9d1
chunghee <boodooboo100@naver.com>
2025-04-18 16:43:51 +0900

    [refactor] CommandBuffer 연동 기능 적용

--- AClass_SSD/Command.h
+++ AClass_SSD/Command.h
@@ -3,19 +3,18 @@
 #include <string>
 #include "SSDControllerInterface.h"
 #include "FileTextIOInterface.h"
+#include "CommandBuffer.h"
 
 class Command {
-public:
-	Command(std::shared_ptr<SSDControllerInterface> ssdData,
-		std::shared_ptr<FileTextIOInterface> outputFile);
-
-	void execute(const std::string& cmdType, int lba, const std::string& valueHex = "");
-
 private:
-	void executeWrite(int lba, const std::string& valueHex);
-	void executeRead(int lba);
-	void executeErase(int lba, int size);
+    std::shared_ptr<SSDControllerInterface> ssd;
+    std::shared_ptr<FileTextIOInterface> outputFile;
+    std::shared_ptr<CommandBuffer> buffer;
 
-	std::shared_ptr<SSDControllerInterface> ssd;
-	std::shared_ptr<FileTextIOInterface> outputFile;
+public:
+    Command(std::shared_ptr<SSDControllerInterface> ssd,
+        std::shared_ptr<FileTextIOInterface> outputFile,
+        std::shared_ptr<CommandBuffer> buffer);
+
+    void execute(const std::string& cmdType, int lba, const std::string& valueHex = "");
 };
