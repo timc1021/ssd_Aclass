6f84da10220ecd212fc869e2f53771bc34672b5e
yujung.choi <unichoi92@hanmail.net>
2025-04-21 14:46:24 +0900

    [refac] testshell init 수행 시 exception 처리되도록 추가

--- Test_Shell/TestShellApp.h
+++ Test_Shell/TestShellApp.h
@@ -9,9 +9,8 @@ public:
 	TestShellApp(ITestShell* testShell) : testShell(testShell) {}
 
 	void run(std::istream& in = std::cin, std::ostream& out = std::cout);
-	void init();
 	void runner(char* argv);
+	bool init();
 
 	ITestShell* testShell;
-	//std::vector<HMODULE> loadedDlls;  // TestShellApp의 멤버로 선언
 };
