58a1eb95a914ba4eb79e8ea2ff546d86cf8035dc
yujung <unichoi92@hanmail.net>
2025-04-22 00:12:08 +0900

    [refac] Logger Mode 설정으로 runner 수행시 print안되도록 하기 - singleton pattern 적용

--- Test_Shell/TestShellApp.cpp
+++ Test_Shell/TestShellApp.cpp
@@ -104,6 +104,8 @@ void TestShellApp::runner(char* argv)
     if (result == false)
         return;
 
+    Logger::getInstance().setMode(1);
+
     std::ifstream file(file_name);
     if (!file.is_open()) {
         std::cerr << "Failed to open " << file_name << std::endl;
@@ -114,12 +116,12 @@ void TestShellApp::runner(char* argv)
         getline(file, command);
         std::cout << command << "---   RUN   ...";
 
-        std::streambuf* originalCoutBuffer = std::cout.rdbuf();
-        std::cout.rdbuf(nullptr);
+        //std::streambuf* originalCoutBuffer = std::cout.rdbuf();
+        //std::cout.rdbuf(nullptr);
 
         result = testShell->handleCommand(command);
 
-        std::cout.rdbuf(originalCoutBuffer);
+        //std::cout.rdbuf(originalCoutBuffer);
 
         if (result == COMMAND_SUCCESS) {
             std::cout << "Pass\n";
